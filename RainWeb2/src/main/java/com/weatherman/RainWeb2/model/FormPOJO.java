6db9a7e1186f25d391eddc60cbd5625ae88bfc5d
ra1n80y <godfreyachu1@gmail.com>
2024-06-10 22:25:59 +0200

    Add files via upload

---
+++ RainWeb2/src/main/java/com/weatherman/RainWeb2/model/FormPOJO.java
@@ -0,0 +1,15 @@
+package com.weatherman.RainWeb2.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class FormPOJO
+{
+    private int ID;
+    private String firstName;
+    private String lastName;
+}
